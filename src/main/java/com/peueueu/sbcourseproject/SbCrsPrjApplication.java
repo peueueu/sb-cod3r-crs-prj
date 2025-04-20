950b734df16fadbc3a34f9d17862bb7806634ba2
peueueu <silvasantospedro91@gmail.com>
2025-04-20 12:36:25 -0300

    initial project setup using spring initializr

---
+++ src/main/java/com/peueueu/sbcourseproject/SbCrsPrjApplication.java
@@ -0,0 +1,13 @@
+package com.peueueu.sbcourseproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbCrsPrjApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbCrsPrjApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/peueueu/sbcourseproject/SbCrsPrjApplicationTests.java
@@ -0,0 +1,13 @@
+package com.peueueu.sbcourseproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbCrsPrjApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
